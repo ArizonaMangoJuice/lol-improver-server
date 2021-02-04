42a3b880d81591032fb525c576d858f7c2272cb5
AMJ <isaelwebdeveloper@gmail.com>
2021-02-03 21:12:48 -0700

    updated config to see env variables

--- models/champions.js
+++ models/champions.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const championSchema = new mongoose.Schema({
     version: {type: String, require: true},
-    id: {type: String, required:true},
+    // id: {type: String, required:true},
     key: {type: String, required:true},
     name: {type: String, required:true},
     title: {type: String, required:true},
@@ -14,9 +14,9 @@ const championSchema = new mongoose.Schema({
     tags: {type: Array, required:true},
     partype: {type: String, required:true},
     stats: {type: Object, required:true},
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
-    queryName: {type: String, required: true},
-    content: String
+    // userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
+    // queryName: {type: String, required: true},
+    // content: String
 },{timestamps: true});
 //reference notes
 championSchema.set('toObject', {
